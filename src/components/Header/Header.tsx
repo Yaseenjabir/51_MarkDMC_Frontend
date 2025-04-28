b9e11f5bb92e98dd836a5d5c27cef16c4e758f7a
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-04-28 22:17:12 +0500

    Removed blog route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     { id: 1, title: "Home", link: "/" },
     { id: 2, title: "About", link: "/about" },
     { id: 3, title: "Services", link: "/services" },
-    { id: 4, title: "Blog", link: "/blog" },
+    // { id: 4, title: "Blog", link: "/blog" },
     { id: 5, title: "Contact", link: "/contact" },
     { id: 6, title: "Portal", link: "/portal" },
   ];
