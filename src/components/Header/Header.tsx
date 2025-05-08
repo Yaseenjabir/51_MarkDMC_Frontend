d335bf0cd55e4aaadd416944cda958e7448e008d
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-08 14:47:52 +0500

    init

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MobileHeader from "./MobileHeader";
 import DesktopHeader from "./DesktopHeader";
+import Script from "next/script";
 
 export default function Header() {
   const navLinks = [
@@ -14,6 +15,26 @@ export default function Header() {
 
   return (
     <>
+      <Script
+        id="tawk-to"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+         
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/681c7a924c5e10190a22d1d4/1iqnjtju3';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+          `,
+        }}
+      />
       <DesktopHeader navLinks={navLinks} />
       <MobileHeader navLinks={navLinks} />
     </>
